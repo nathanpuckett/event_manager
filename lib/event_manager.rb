cb57b0a4bff3985530134767bf9b03388a111d49
nathanpuckett <nathanfpuckett@gmail.com>
2023-03-22 19:57:21 -0500

    Implement Ruby CSV library to parse data

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-puts 'Event Manager Initialized!'
+require 'csv'
+puts 'EventManager Initialized.'
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index.zero?
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
 
-  columns = line.split(',')
-  name = columns[2]
-  puts name
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
