a3b38c8aed991f54f358926e2eeaf7df48c1410c
nathanpuckett <nathanfpuckett@gmail.com>
2023-03-22 20:11:21 -0500

    Add clean_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'EventManager Initialized.'
 
 contents = CSV.open(
@@ -11,6 +16,8 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
